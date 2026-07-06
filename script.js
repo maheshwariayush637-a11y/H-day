b8cba27b76543aac43a925767f50af6b7f75958d
maheshwariayush637-a11y <maheshwariayush637@gmail.com>
2026-07-06 09:18:39 +0530

    Update yesTeasePokes array with new messages

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ const noMessages = [
 ]
 
 const yesTeasePokes = [
-    "try saying no first... I bet you want to know what happens 😏",
-    "go on, hit no... just once 👀",
-    "you're missing out 😈",
-    "click no, I dare you 😏"
+    "try saying no first...",
+    "Go on, hit no... just once 👀",
+    "No Pai click kr toh sahiii",
+    "Aur kitna Yes kregi... NO pai click kr"
 ]
 
 let yesTeasedCount = 0
